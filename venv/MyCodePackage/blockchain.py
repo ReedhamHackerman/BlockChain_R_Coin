30e6d01dc70f79ada5e848386aaef6c23c7ec98e
ReedhamHackerman <48519104+ReedhamHackerman@users.noreply.github.com>
2021-05-20 22:18:24 -0400

    Proof Of work Adding Proof Of work to verify the previous hash

--- venv/MyCodePackage/blockchain.py
+++ venv/MyCodePackage/blockchain.py
@@ -18,3 +18,18 @@ class BlockChain:
         }
         self.chain.append(block)
         return block
+
+    def get_previous_block(self):
+        return self.chain[-1]
+
+    @staticmethod
+    def proof_of_work(previous_proof):
+        new_proof = 1
+        check_proof = False
+        while check_proof is False:
+            hash_operation = hashlib.sha256(str(new_proof ** 2 - previous_proof ** 2).encode()).hexdigest()
+            if hash_operation[0:4] == '0000':
+                check_proof = True
+            else:
+                new_proof += 1
+        return new_proof
