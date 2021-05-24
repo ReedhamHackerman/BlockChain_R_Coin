c59917003c4fdc3d834331c98a5375e30bc9576b
ReedhamHackerman <48519104+ReedhamHackerman@users.noreply.github.com>
2021-05-24 07:33:16 -0400

    Making Flask Application
    Making a Flask application that checks the status of the code and return in the new dictionary .

--- venv/MyCodePackage/blockchain.py
+++ venv/MyCodePackage/blockchain.py
@@ -54,3 +54,22 @@ class BlockChain:
             block_index += 1
         return True
 
+
+app = Flask(__name__)
+blockchain = BlockChain()
+
+
+@app.route('/mine_block', methods=['GET'])
+def mine_block():
+    previous_block = blockchain.get_previous_block()
+    previous_proof = previous_block['proof']
+    proof = blockchain.proof_of_work(previous_proof)
+    previous_hash = blockchain.hash(previous_block)
+    block = blockchain.create_block(proof, previous_hash)
+    response = {'message': 'Congratulations,You just mine the block',
+                'index': block['index'],
+                'timestamp': block['timestamp'],
+                'proof': block['proof'],
+                'previous_hash': block['previous_hash']
+                }
+    return jsonify(response), 200
