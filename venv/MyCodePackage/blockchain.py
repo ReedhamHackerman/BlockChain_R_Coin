922d753b509a8ada06ee39601498359af14ca838
ReedhamHackerman <48519104+ReedhamHackerman@users.noreply.github.com>
2021-05-24 07:39:07 -0400

    Creating a Chain for the block

--- venv/MyCodePackage/blockchain.py
+++ venv/MyCodePackage/blockchain.py
@@ -73,3 +73,10 @@ def mine_block():
                 'previous_hash': block['previous_hash']
                 }
     return jsonify(response), 200
+
+
+@app.route('/get_chain', methods=['GET'])
+def get_chain():
+    response = {'chain': blockchain.chain,
+                'length': len(blockchain.chain)}
+    return jsonify(response), 200
